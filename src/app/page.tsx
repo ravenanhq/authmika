e204fdc9ef34c635028e64ef1d370f94dc864740
vsaravanakumar <saravanan@kuruntham.com>
2023-12-11 12:37:46 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Authmika</h1>
+    </main>
+  )
+}
