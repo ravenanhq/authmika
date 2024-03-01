f850e04f65c1d62f1148771002649f7b5637f94f
vsaravanakumar <saravanan@kuruntham.com>
2024-03-01 18:56:52 +0530

    Task#50 - API created to user CRUD

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Users } from '../db/model/users.model';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [SequelizeModule.forFeature([Users])],
   providers: [UsersService],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
